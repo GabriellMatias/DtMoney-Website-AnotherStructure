2eb37816a12ad58144a81c0b84306037038279fc
GabriellMatias <gabriel.goiasat@gmail.com>
2022-11-26 08:09:09 -0300

    :tada: Finished page styling

--- src/Components/NewTransactionModal/index.tsx
+++ src/Components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Content, Overlay } from "./style";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./style";
 
 export function NewTransactionModal() {
   return (
@@ -7,16 +8,31 @@ export function NewTransactionModal() {
       <Overlay />
       <Content>
         <Dialog.Title>New Transaction</Dialog.Title>
+        <CloseButton>
+          <X size={24}/>
+        </CloseButton>
 
         <form action="">
-          <input type="text" placeholder="description" required/>
-          <input type="number" placeholder="Price" required/>
-          <input type="text" placeholder="Category" required/>
-        
-        <button type="submit">Register</button>
-        </form> 
+          <input type="text" placeholder="description" required />
+          <input type="number" placeholder="Price" required />
+          <input type="text" placeholder="Category" required />
 
-        <Dialog.Close />
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24}/>
+              Incoming
+            </TransactionTypeButton>
+            
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24}/>
+              Outcoming
+            </TransactionTypeButton>
+
+          </TransactionType>
+
+
+          <button type="submit">Register</button>
+        </form>
       </Content>
     </Dialog.Portal>
   );
